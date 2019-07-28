0b94fa51f9352b8ca2adb49b5a37e9072ac4bca7
Luke Rogerson <lukerogerson@aol.com>
2019-07-28 21:22:59 +0100

    Hello world example

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,10 @@
-module.exports.handler = (evt, ctx, done) => {
-  
-}
+module.exports.hello = (evt, ctx, done) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Helloololololololo!',
+      input: evt
+    })
+  };
+  done(null, response);
+};
